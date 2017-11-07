34deb7f565265848ad9f7df21bbe034e2fd2324d
Michelle Cinello <m.cinello@gmail.com>
2017-11-07 10:50:18 -0500

    practicing converting ruby code to js

---
+++ ruby-to-js.js
@@ -0,0 +1,17 @@
+var apples = 14;
+console.log(apples);
+
+console.log('I have ' + apples + ' apples');
+
+var materials = ['wood', 'metail', 'stone'];
+
+var words = {
+  "elephant": "The world's largest land mammal.",
+  "school": "A place of learning.",
+  "ice cream": "A delicious milk-based dessert.",
+};
+
+var num = 16;
+  if {
+    
+  };
