55c2f3de21c8d1f4b2ab8e44033cea6cb42c055f
Michelle Cinello <m.cinello@gmail.com>
2017-11-07 15:00:51 -0500

    get destination and fill up gas

--- autopilot.js
+++ autopilot.js
@@ -16,8 +16,30 @@ function pickUpPassenger(car) {
   console.log("Picked up passenger. Car now has " + car["passengers"] + " passengers.");
 }
 
+function getDestination(car) {
+  if (car["city"] === 'Toronto') {
+    console.log("Mississauga");
+  } else if (car["city"] === 'Mississauga') {
+    console.log("London");
+  } else if (car["city"] === 'London') {
+    console.log("Toronto");
+  }
+}
+
+function fillUpGas(car) {
+  var old_gas = car["gas"];
+  car["gas"] = 100;
+  console.log("Filled up to " + car["gas"] + " on gas from " + getGasDisplay(old_gas));
+}
+
+function getGasDisplay(gasAmount) {
+  return gasAmount
+}
+
+
 addCar(cars, getNewCar);
 console.log(cars);
-// console.log(getNewCar());
 pickUpPassenger(cars[0])
 console.log(cars);
+getDestination(cars[0])
+fillUpGas(cars[0])
