64dd8561960d375cae0178ccf25e40ca1970d7f8
Michelle Cinello <m.cinello@gmail.com>
2017-11-07 16:18:49 -0500

    fixed the bug causing undefined to appear

--- autopilot.js
+++ autopilot.js
@@ -1,19 +1,22 @@
-var getNewCar = {
-  "city": "Toronto",
-  "passengers": 0,
-  "gas": 100
+function getNewCar() {
+  return {
+    "city": "Toronto",
+    "passengers": 0,
+    "gas": 100
   }
+}
 
 var cars = []
-function addCar(cars, getNewCar) {
-  cars.push(getNewCar)
-  console.log("Adding new car to fleet. Fleet size is now " + cars.length + "." );
+
+function addCar(cars, carPassedIn) {
+  cars.push(carPassedIn)
+  return "Adding new car to fleet. Fleet size is now " + cars.length + ".";
 }
 
 function pickUpPassenger(car) {
   car["passengers"] += 1;
   car["gas"] -= 10;
-  console.log("Picked up passenger. Car now has " + car["passengers"] + " passengers.");
+  return "Picked up passenger. Car now has " + car["passengers"] + " passengers.";
 }
 
 function getDestination(car) {
@@ -29,7 +32,7 @@ function getDestination(car) {
 function fillUpGas(car) {
   var oldGas = car["gas"];
   car["gas"] = 100;
-  console.log("Filled up to " + car["gas"] + " on gas from " + getGasDisplay(oldGas));
+  return "Filled up to " + car["gas"] + " on gas from " + getGasDisplay(oldGas)
 }
 
 function getGasDisplay(gasAmount) {
@@ -38,44 +41,64 @@ function getGasDisplay(gasAmount) {
 
 function drive(car, city_distance) {
   if (car["gas"] < city_distance) {
-    fillUpGas(car);
+    return fillUpGas(car);
   }
 
   car["city"] = getDestination(car);
   car["gas"] -= city_distance;
-  console.log("Drove to " + car["city"] + ". Remaining gas: " + getGasDisplay(car["gas"]));
+  return "Drove to " + car["city"] + ". Remaining gas: " + getGasDisplay(car["gas"]);
 }
 
 function dropOffPassengers(car) {
   var previousPax = car["passengers"];
   car["passengers"] = 0;
-  console.log("Dropped off " + previousPax + " passengers.");
+  return "Dropped off " + previousPax + " passengers.";
 }
 
 function act(car) {
     var distanceBetweenCities = 50
 
     if (car["gas"] < 20) {
-      return fillUpGas(car)
+      return fillUpGas(car);
     } else if (car["passengers"] < 3) {
-      return pickUpPassenger(car)
+      return pickUpPassenger(car);
     } else {
       if (car["gas"] < distanceBetweenCities) {
-        var droveTo = drive(car, distanceBetweenCities);
-        var paxDropped = dropOffPassengers(car);
-        console.log(droveTo + paxDropped);
       }
+      var droveTo = drive(car, distanceBetweenCities);
+      var paxDropped = dropOffPassengers(car);
+      return droveTo + paxDropped;
     }
+}
+
+function commandFleet(cars) {
+  for (var i = 0; i < cars.length; i++) {
+    var action = act(cars[i]);
+    console.log("Car " + (i+1) + ": " + action);
+  }
+  console.log("---");
+}
+
+function addOneCarPerDay(cars, numDays) {
+  for (var i = 0; i < numDays; i++) {
+    var newCar = getNewCar();
+    var result = addCar(cars, newCar);
+    console.log(result);
+    commandFleet(cars);
+  }
 }
 
 
 
-addCar(cars, getNewCar);
+addCar(cars, getNewCar());
 console.log(cars);
-pickUpPassenger(cars[0])
+pickUpPassenger(cars[0]);
 console.log(cars);
-getDestination(cars[0])
-fillUpGas(cars[0])
-drive(cars[0], 20)
-dropOffPassengers(cars[0])
-act(cars[0])
+getDestination(cars[0]);
+fillUpGas(cars[0]);
+drive(cars[0], 20);
+dropOffPassengers(cars[0]);
+act(cars[0]);
+commandFleet(cars);
+console.log("----------------------");
+addOneCarPerDay(cars, 10)
