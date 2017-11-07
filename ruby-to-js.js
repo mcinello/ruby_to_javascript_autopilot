9d40bafafda53aa82f83cb99595a27c701dde5bb
Michelle Cinello <m.cinello@gmail.com>
2017-11-07 11:53:54 -0500

    repeat log 10 times

--- ruby-to-js.js
+++ ruby-to-js.js
@@ -14,8 +14,15 @@ var words = {
 var num = 16;
   if (num > 10) {
     console.log(num + " is greater than 10");
-  } else if (num == 10) {
+  } else if (num === 10) {
     console.log(num + " is exactly 10");
   } else {
     console.log(num + " must be less than 10");
   }
+
+// 10 times do
+
+var repeat = 10;
+for (var i=0; i < repeat; i++) {
+  console.log("Doing the same thing over and over.");
+}
