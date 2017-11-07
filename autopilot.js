b3e1ef9a9630b60b6ac3f0965a9a4b33a7a2b6ca
Michelle Cinello <m.cinello@gmail.com>
2017-11-07 14:52:22 -0500

    edited the function getNewCar to a var, pickUpPassenger changes values in objects

--- autopilot.js
+++ autopilot.js
@@ -1,16 +1,23 @@
-function getNewCar() {
-  var info = {
+var getNewCar = {
   "city": "Toronto",
   "passengers": 0,
   "gas": 100
   }
-  console.log(info);
-}
-getNewCar()
 
 var cars = []
-function addCar(cars, new_car) {
-  cars.push(new_car)
+function addCar(cars, getNewCar) {
+  cars.push(getNewCar)
   console.log("Adding new car to fleet. Fleet size is now " + cars.length + "." );
 }
-addCar(cars, "Ferrari")
+
+function pickUpPassenger(car) {
+  car["passengers"] += 1;
+  car["gas"] -= 10;
+  console.log("Picked up passenger. Car now has " + car["passengers"] + " passengers.");
+}
+
+addCar(cars, getNewCar);
+console.log(cars);
+// console.log(getNewCar());
+pickUpPassenger(cars[0])
+console.log(cars);
